87be9bc64672a0d70d523b0293dcddea9f5eeb13
Chungsing Arguello <chung@Chungsings-iMac.local>
2020-02-07 19:22:26 -0500

    member class and memberaccount class done

--- src/main/java/com/pokersandbox/PokerSandbox/repositories/UserRepo.java
+++ src/main/java/com/pokersandbox/PokerSandbox/repositories/UserRepo.java
@@ -1,12 +1,12 @@
 package com.pokersandbox.PokerSandbox.repositories;
 
-import com.pokersandbox.PokerSandbox.models.User;
+import com.pokersandbox.PokerSandbox.models.Member;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepo  extends CrudRepository <User, Long> {
+public interface UserRepo  extends CrudRepository <Member, Long> {
 
-    User findByUsername(String username);
+    Member findByUsername(String username);
     Boolean existsByUsername(String username);
 }
