ba659c7dbc9cddb6ba5133d8bfef15c4666cdc26
Chungsing Arguello <chung@Chungsings-iMac.local>
2020-02-11 14:09:21 -0500

    register method added to memberservice class

--- src/main/java/com/pokersandbox/PokerSandbox/services/MemberService.java
+++ src/main/java/com/pokersandbox/PokerSandbox/services/MemberService.java
@@ -2,14 +2,54 @@ package com.pokersandbox.PokerSandbox.services;
 
 import com.pokersandbox.PokerSandbox.models.Member;
 import com.pokersandbox.PokerSandbox.models.dto.MemberRegister;
+import com.pokersandbox.PokerSandbox.repositories.MemberRepo;
+import com.pokersandbox.PokerSandbox.utilities.Validators;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.Date;
 
 @Service
 public class MemberService {
 
+    private final MemberRepo memberRepo;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public MemberService (MemberRepo memberRepo, PasswordEncoder passwordEncoder){
+        this.memberRepo = memberRepo;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    /**
+     * Register Method for new member with password validator.
+     */
     public String registerMember(MemberRegister newMember) {
         Member member = new Member();
+        Date date = new Date();
 
-        return null;
+        if(!Validators.validateEmail(newMember.getEmail()))
+            return "Not a valid Email, Please use a valid email";
+
+        if(memberRepo.existsByEmail(newMember.getEmail()))
+            return "The email " + newMember.getEmail() + " is already in use, please log in";
+
+        if(memberRepo.existsByUsername(newMember.getUsername()))
+            return "The username is already in use";
+
+        if(Validators.validatePassword(newMember.getPassword())) {
+            member.setUsername(newMember.getUsername());
+            member.setPassword(passwordEncoder.encode(newMember.getPassword()));
+            member.setEmail(newMember.getEmail());
+
+            LocalDate today = LocalDate.now();
+            member.setDate_registered(today);
+
+            memberRepo.save(member);
+        }
+
+        return "Account created";
     }
 }
